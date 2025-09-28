46c84d8e9465546d0ef58c5729202373bf73ace9
Mirror83 <glenochieng045@gmail.com>
2025-09-28 11:32:39 +0300

    test: add additional scenarios for calculation function tests

--- src/__tests__/tip-calculation.test.ts
+++ src/__tests__/tip-calculation.test.ts
@@ -10,11 +10,27 @@ describe("tip calculation", () => {
     // The values here are based on the example in the design
     expect(calculateTipPerPerson(142.55, 15, 5)).toBeCloseTo(4.2765);
   });
+  it("should return 0 and not crash with division by zero error when zero is passed in for number of people", () => {
+    expect(calculateTipPerPerson(100, 15, 0)).toBe(0);
+  });
+  it("should handle null inputs gracefully", () => {
+    expect(calculateTipPerPerson(null, 15, 5)).toBe(0);
+    expect(calculateTipPerPerson(100, null, 5)).toBe(0);
+    expect(calculateTipPerPerson(100, 15, null)).toBe(0);
+  });
 });
 
 describe("total bill calculation", () => {
   it("should calculate total bill per person correctly", () => {
     // The values here are based on the example in the design
     expect(calculateTotalBillPerPerson(142.55, 4.2765, 5)).toBeCloseTo(32.7865);
+  });
+  it("should return 0 and not crash with division by zero error when zero is passed in for number of people", () => {
+    expect(calculateTotalBillPerPerson(100, 15, 0)).toBe(0);
+  });
+  it("should handle null inputs gracefully", () => {
+    expect(calculateTotalBillPerPerson(null, 15, 5)).toBe(0);
+    expect(calculateTotalBillPerPerson(100, null, 5)).toBe(0);
+    expect(calculateTotalBillPerPerson(100, 15, null)).toBe(0);
   });
 });
